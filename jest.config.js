66c3318b1bed0ff7b46a0295bc82801fe414449a
Nadir1109 <i522995@FontysICT.nl>
2024-10-15 01:40:27 +0200

    added test via cmd and unit test

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/no-babel'
-}
+  preset: '@vue/cli-plugin-unit-jest/presets/no-babel',
+  testMatch: ['**/tests/unit/**/*.spec.[jt]s?(x)'],
+  testEnvironment: 'jsdom', // Voeg dit toe als je een DOM-gebaseerde omgeving nodig hebt voor Vue.js componenten
+};
